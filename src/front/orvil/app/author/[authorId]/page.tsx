1001049eb3b699212af4f7220be9023628071103
nakayama-luiz <luizsantos.naka@gmail.com>
2024-07-07 22:49:43 -0300

    cards nunca estiveram melhores.

--- src/front/orvil/app/author/[authorId]/page.tsx
+++ src/front/orvil/app/author/[authorId]/page.tsx
@@ -1,7 +1,7 @@
 import { author } from "@/types/author";
 import { IAuthorEditionCard } from "@/types/AuthorEditionCard";
 import { Image } from "@nextui-org/image";
-import { Card, CardHeader, CardBody } from "@nextui-org/card";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import { wrap } from "module";
 
 async function getAuthorData(params: number): Promise<author> {
@@ -65,11 +65,12 @@ export default async function main({
         <div className="grid grid-rows-3 grid-flow-col gap-4">
           <div className="row-span-3 ...">
             <Image isBlurred width={300} src={authorData.picture}></Image>
-            <label htmlFor="">{authorData.vulgos}</label>
+            {/* <label htmlFor="">{authorData.vulgos}</label> */}
           </div>
 
           <div className="col-span-2 ...">
             <h1 style={{ fontSize: 30 }}>{authorData.name}</h1>
+            <small className="text-default-500">{authorData.vulgos}</small>
           </div>
 
           <div className="row-span-2 col-span-2 ...">
@@ -78,7 +79,25 @@ export default async function main({
         </div>
         <hr />
         <br />
+        <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+  {authorEditions.map((item, index) => (
+    <Card shadow="sm" key={index} isPressable>
+      <CardBody className="flex flex-col justify-center items-center overflow-visible p-0 px-2 py-1">
+        <Image
+          shadow="sm"
+          radius="lg"
+          alt={item.editiontitle}
+          className="w-auto h-[200px] object-cover aspect-auto"
+          src={item.cover}
+        />
+      </CardBody>
+      <CardFooter className="justify-center items-center flex">
 
+        <h1 className="text-xs text-center">{item.editiontitle}</h1>
+      </CardFooter>
+    </Card>
+  ))}
+</div>
         {/* <div>
           {authorEditions.map((ele) => (
             <div>
@@ -101,29 +120,51 @@ export default async function main({
           ))}
         </div> */}
 
-
-<div className="cards-container" style={{display: 'flex', flexWrap: 'wrap', gap: 10}}>
-  {authorEditions.map((ele) => (
-    <div className="card-wrapper" style={{display: 'inline-block', width: 'auto', maxWidth: 300}}>
-      <Card className="py-4 card-content">
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <p className="text-tiny uppercase font-bold">Daily Mix</p>
-          <small className="text-default-500">{ele.authorname}</small>
-          <h4 className="font-bold text-large">{ele.editiontitle}</h4>
-        </CardHeader>
-        <CardBody className="overflow-visible py-2">
-          <Image
-            alt="Card background"
-            className="object-cover rounded-xl"
-            src={ele.cover}
-            width={100}
-          />
-        </CardBody>
-      </Card>
-    </div>
-  ))}
-</div>
-
+        <div
+          className="cards-container"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: 10,
+            justifyContent: "center",
+          }}
+        >
+          {authorEditions.map((ele) => (
+            <div
+              className="card-wrapper"
+              style={{
+                display: "inline-block",
+                flex: 1,
+                maxWidth: 250,
+                margin: 10,
+              }}
+            >
+              <Card className="py-4 card-content">
+                <CardHeader className="pb-0 pt-2 px-4 flex-col items-center justify-center flex">
+                  {/* <p className="text-tiny uppercase font-bold">Daily Mix</p> */}
+                  {/* <small className="text-default-500">{ele.authorname}</small> */}
+                  <h4 className="font-bold text-large">{ele.editiontitle}</h4>
+                </CardHeader>
+                <CardBody className="overflow-visible py-2">
+                  <div
+                    style={{
+                      justifyContent: "center",
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Image
+                      alt="Card background"
+                      className="object-cover rounded-xl"
+                      src={ele.cover}
+                      width={100}
+                    />
+                  </div>
+                </CardBody>
+              </Card>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/front/orvil/components/navbar.tsx
+++ src/front/orvil/components/navbar.tsx
@@ -54,7 +54,7 @@ export const Navbar = () => {
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">ORVIL</p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -77,8 +77,7 @@ export const Navbar = () => {
 
       <NavbarContent
         className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
+        justify="end">
         <NavbarItem className="hidden sm:flex gap-2">
           <Link isExternal aria-label="Twitter" href={siteConfig.links.twitter}>
             <TwitterIcon className="text-default-500" />
@@ -86,13 +85,13 @@ export const Navbar = () => {
           <Link isExternal aria-label="Discord" href={siteConfig.links.discord}>
             <DiscordIcon className="text-default-500" />
           </Link>
-          <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+          {/* <Link isExternal aria-label="Github" href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
-          </Link>
+          </Link> */}
           <ThemeSwitch />
         </NavbarItem>
         <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
+        {/* <NavbarItem className="hidden md:flex">
           <Button
             isExternal
             as={Link}
@@ -103,7 +102,7 @@ export const Navbar = () => {
           >
             Sponsor
           </Button>
-        </NavbarItem>
+        </NavbarItem> */}
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
